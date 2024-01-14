24f670012cb988f6bbfcf8d208659969bcd73ef1
Brianna <TheEnigmaticHaze@gmail.com>
2024-01-14 10:51:25 -0500

    we do a little refactoring (and fixing)

--- 2024Problem.py
+++ 2024Problem.py
@@ -1,6 +1,8 @@
 from functools import cache
 from itertools import product
-OPERATION_MAX = 8
+from memory_profiler import profile 
+
+OPERATION_MAX = 6
 NUMBERS = [2, 0, 2, 4]
 
 POSSIBLE_CONCATENATIONS = [
@@ -35,6 +37,23 @@ OP_LAMBDAS = {"id"  : (lambda v1     : v1                  ), # identity/no-op
               "!!"  : (lambda v1     : double_factorial(v1)), # double factorial
               "sqrt": (lambda v1     : v1 ** .5            )} # square root
 
+def prefix_to_infix(stack):
+  output_stack = []
+  for element in reversed(stack):
+    if element not in OPERATIONS.keys():
+      output_stack.append(element)
+    elif OPERATIONS[element] == 2:
+      output_stack.append(f"({output_stack.pop()} {element} {output_stack.pop()})")
+    elif element == "~":
+      output_stack.append(f"-{output_stack.pop()}")
+    elif element == "sqrt":
+      output_stack.append(f"sqrt({output_stack.pop()})")
+    elif element == "id":
+      output_stack.append(f"{output_stack.pop()}")
+    else:
+      output_stack.append(f"({output_stack.pop()}{element})")
+  return output_stack[0][1:-1]
+
 @cache
 def factorial(x : int) -> int:
   if x < 0 or x > 20:
@@ -49,7 +68,7 @@ def double_factorial(x : int) -> int:
     raise INVALID_STACK
   if x == 0 or x == 1:
     return 1
-  return x * factorial(x - 2)
+  return x * double_factorial(x - 2)
 
 @cache
 def power(x : int, y : int) -> int:
@@ -88,21 +107,25 @@ def eval_stack(stack) -> int:
       raise INVALID_STACK
   
   return num_stack[0]
-    
-def try_stack(stack):
+
+def try_stack(stack : list):
   starting_stack = stack.copy()
 
   try:
     ans = eval_stack(stack)
     if ans in to_find:
-      print(ans, starting_stack)
+      print(ans, prefix_to_infix(starting_stack))
       to_find.discard(ans)
+      del starting_stack
   except KeyboardInterrupt:
     quit()
   except:
+    del starting_stack
     return
 
 def get_next_operation_list(operation_list):
+  if "sqrt" in set(operation_list) and len(set(operation_list)) == 1:
+    return []
   op_value = 0
   place_value = 0
 
@@ -112,40 +135,44 @@ def get_next_operation_list(operation_list):
   
   to_return = []
   op_value += 1
-  for current_place_value in range(place_value):
-    to_return.append(list(OPERATIONS.keys())[(op_value // (10 ** current_place_value)) % 10])
+  for i in range(place_value):
+    to_return.append(list(OPERATIONS.keys())[op_value % 10])
+    op_value //= 10
   
   return tuple(reversed(to_return))
 
+def generate_stack(numbers, current_op_list, grouping):
+  number_index = 0
+  group_index = 0
+  operation_index = 0
+  
+  to_return = []
+  while len(to_return) != len(numbers) + OPERATION_MAX:
+    to_return.append(current_op_list[operation_index])
+    operation_index += 1
 
+    for i in range(grouping[group_index]):
+      to_return.append(numbers[number_index])
+      number_index += 1
+    group_index += 1
+  return to_return
 
+@profile
 def solve(numbers):
-  current_op_list = tuple(["id"] * OPERATION_MAX)
+  current_op_list = ["id"] * OPERATION_MAX
+  current_op_list = tuple(current_op_list)
 
   group_numbers = [x for x in product(range(len(numbers)), repeat=OPERATION_MAX) if sum(x) == len(numbers) and x[-1] != 0]
-  print(len(group_numbers))
+
   while len(current_op_list) == OPERATION_MAX:
     current_op_list = get_next_operation_list(current_op_list)
     if sum(OPERATIONS[op] - 1 for op in current_op_list) + 1 != len(numbers):
       continue
 
     for grouping in group_numbers:
-      number_index = 0
-      group_index = 0
-      operation_index = 0
-
-      stack = []
-
-      while len(stack) != len(numbers) + OPERATION_MAX:
-        stack.append(current_op_list[operation_index])
-        operation_index += 1
-
-        for i in range(grouping[group_index]):
-          stack.append(numbers[number_index])
-          number_index += 1
-        group_index += 1
+      stack = generate_stack(numbers, current_op_list, grouping)
       try_stack(stack)
 
 for nums in POSSIBLE_CONCATENATIONS:
   solve(nums)
-print(to_find)
+print(f"unfound numbers: {to_find}")
